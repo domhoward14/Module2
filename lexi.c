6cbd94a3f436db8ca79fe5ba06e533d677054bc6
Eric Howard <domhoward14@gmail.com>
2015-06-08 14:35:18 -0400

    was trying to eliminate all of the comments before putting it to the code array

--- lexi.c
+++ lexi.c
@@ -10,9 +10,14 @@ typedef struct Token
 
 void load();
 int isSymbol(int x);
+void prinCleanInput();
+int isEnd(char string []);
+void skipComment();
 
 FILE *codeFile;
+FILE *output;
 int structIndex = 0;
+int codeCount = 0;
 Token *tokenArray;
 ArrayList *codeArray;
 
@@ -34,19 +39,22 @@ void initArrays()
 
 int main()
 {
+    codeFile = fopen("input.txt", "r");
+    output = fopen("cleaninput.txt", "w");
     initArrays();
-
     load();
+    prinCleanInput();
     return 0;
 }
 
+//function responsible for retrieving data from the input file and then loading it to the code array for
+//code output and further token processing.
 void load()
 {
     char buffer[256];
     char symbolBuffer[2];
     int i = 0;
     int x, prev = 0, codeIndex;
-    codeFile = fopen("input.txt", "r");
     if(!codeFile)
     {
         printf("Error in opening the file.");
@@ -55,13 +63,28 @@ void load()
     }
     while ((x = fgetc(codeFile)) != EOF)
     {
+        //printf("this iteration of x is %c \n", x);
+        char tempString [10];
+
         if(isSymbol(x))
         {
-            if(!isSymbol(prev))
+            if(x == '*' && prev == '/')
+            {
+                //skip over comment and continue
+                skipComment();
+                prev = 0;
+                continue;
+            }
+
+            //if(!isSymbol(prev))
                 put(codeArray, buffer);
+
+
             symbolBuffer[0] = x;
             symbolBuffer[1] = '\0';
             put(codeArray, symbolBuffer);
+            if(isEnd(buffer))
+                break;
             i = 0;
         }
         else
@@ -74,6 +97,8 @@ void load()
 
     }
     printArrayList(codeArray);
+    fclose(codeFile);
+
 }
 
 int isSymbol(int x)
@@ -83,4 +108,36 @@ int isSymbol(int x)
         return 1;
     }
     return 0;
+}
+
+void prinCleanInput()
+{
+    int i = 0;
+    char* tempString;
+    while(i <= codeCount)
+    {
+       if((tempString = get(codeArray, i)) == "\n")
+            fprintf(output, "\n");
+        fprintf(output, "%s", tempString);
+        i ++;
+    }
+    fclose(output);
+}
+
+int isEnd (char string [])
+{
+    if (string[0] == 'e' && string[1] == 'n' && string[2] == 'd')
+        return 1;
+    return 0;
+}
+
+void skipComment()
+{
+        int previousPointer = 0, currentPointer = fgetc(codeFile);
+        while (previousPointer != '*' && currentPointer != '/')
+        {
+            previousPointer = currentPointer;
+            currentPointer = fgetc(codeFile);
+        }
+       // fseek(output, -10, SEEK_CUR);
 }
