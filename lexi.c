45888185168fa09c0c1529448bb6c8637c4923c0
Eric Howard <domhoward14@gmail.com>
2015-06-12 04:57:41 -0400

    Finished all of the parsing auxillary functions

--- lexi.c
+++ lexi.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ArrayList.c"
+#include <string.h>
 
 typedef struct Token
 {
@@ -16,6 +17,17 @@ int isEnd();
 void skipComment();
 void printTest();
 int isLetter(char letter);
+void putSymbolToken(char *string, int symbol);
+void putReserveToken(char *string, int symbol);
+void putNumToken(char *string);
+void putIdentifierToken(char *string);
+int symbolSwitch(char *string);
+int isNumber(char *string);
+int wordSwitch(char *string);
+void findToken();
+
+
+
 
 char buffer[256];
 FILE *codeFile;
@@ -48,7 +60,8 @@ int main()
     initArrays();
     load();
     //printCleanInput();
-    //cleanArrayList();
+    cleanArrayList();
+    findToken();
     printTest();
     return 0;
 }
@@ -160,6 +173,8 @@ void skipComment()
 }
 void printTest()
 {
+    findToken();
+    printf("%d, %s", tokenArray[4].sym, tokenArray[4].word );
 }
 
 void cleanArrayList()
@@ -177,45 +192,45 @@ void cleanArrayList()
         }
     }
 }
-/*
+
 void findToken()
 {
-    int i = 0;
+    structIndex = 0;
     char * string;
     char bos;
-    while(i < codeArray ->size)
+    while(structIndex < codeArray ->size)
     {
-            string = get(codeArray, i);
+            string = get(codeArray, structIndex);
             bos = string[0];
             if(isLetter(bos))
             {
-                if(wordSwitch())
+                if(wordSwitch(string))
                 {
-                    index ++
+                    structIndex ++;
                     continue;
                 }
                 else
                 {
-                    putIdentifierToken();
-                    i ++;
+                    putIdentifierToken(string);
+                    structIndex ++;
                     continue;
                 }
             }
             else
             {
-                if(isNumber())
+                if(isNumber(string))
                 {
-                    makeNumToken();
-                    i ++;
+                    putNumToken(string);
+                    structIndex ++;
                     continue;
                 }
                 else
-                    symbolSwitch();
-                    i ++;
+                    symbolSwitch(string);
+                    structIndex ++;
             }
     }
 }
-*/
+
 int isLetter(char letter)
 {
     if(letter >= 65 && letter <= 90)
@@ -224,4 +239,216 @@ int isLetter(char letter)
         return 1;
     else
         return 0;
+}
+
+int wordSwitch(char *string)
+{
+    char bos = string[0];
+    switch(bos)
+    {
+        case 'c':
+            if(strcmp (string, "const") == 0)
+            {
+                putReserveToken("const", 28);
+                break;
+            }
+            if(strcmp (string, "call") == 0)
+            {
+                putReserveToken("call", 27);
+                break;
+            }
+            putIdentifierToken(string);
+            break;
+
+        case 'v':
+            if(strcmp (string, "var") == 0)
+            {
+                putReserveToken("var", 29);
+                break;
+            }
+            putIdentifierToken(string);
+            break;
+
+        case 'p':
+            if(strcmp (string, "procedure") == 0)
+            {
+                putReserveToken("procedure", 30);
+                break;
+            }
+            putIdentifierToken(string);
+            break;
+
+        case 'b':
+            if(strcmp (string, "begin") == 0)
+            {
+                putReserveToken("begin", 21);
+                break;
+            }
+            putIdentifierToken(string);
+            break;
+
+        case 'e':
+            if(strcmp (string, "end") == 0)
+            {
+                putReserveToken("end", 22);
+                break;
+            }
+
+            if(strcmp (string, "else") == 0)
+            {
+                putReserveToken("else", 33);
+                break;
+            }
+            putIdentifierToken(string);
+            break;
+
+        case 'i':
+            if(strcmp (string, "if") == 0)
+            {
+                putReserveToken("if", 23);
+                break;
+            }
+            putIdentifierToken(string);
+            break;
+
+        case 't':
+            if(strcmp (string, "then") == 0)
+            {
+                putReserveToken("then", 24);
+                break;
+            }
+            putIdentifierToken(string);
+            break;
+
+        case 'w':
+            if(strcmp (string, "while") == 0)
+            {
+                putReserveToken("while", 25);
+                break;
+            }
+
+            if(strcmp (string, "write") == 0)
+            {
+                putReserveToken("write", 31);
+                break;
+            }
+            putIdentifierToken(string);
+            break;
+
+        case 'd':
+            if(strcmp (string, "do") == 0)
+            {
+                putReserveToken("do", 26);
+                break;
+            }
+            putIdentifierToken(string);
+            break;
+
+        case 'r':
+            if(strcmp (string, "read") == 0)
+            {
+                putReserveToken("read", 32);
+                break;
+            }
+            putIdentifierToken(string);
+            break;
+
+        case 'o':
+            if(strcmp (string, "odd") == 0)
+            {
+                putReserveToken("odd", 8);
+                break;
+            }
+            putIdentifierToken(string);
+            break;
+
+        default :
+            putIdentifierToken(string);
+
+    }
+}
+
+int isNumber(char *string)
+{
+    int number = string[0];
+
+    if(number >= 48 && number <= 57)
+    {
+        putNumToken(string);
+        return 1;
+    }
+    return 0;
+
+}
+
+int symbolSwitch(char *string)
+{
+    char symbol = string[0];
+    switch(symbol)
+    {
+        case '+':
+            putSymbolToken("+", 4);
+            break;
+        case '-':
+            putSymbolToken("-", 5);
+            break;
+        case '*':
+            putSymbolToken("*", 6);
+            break;
+        case '/':
+            putSymbolToken("/", 7);
+            break;
+        case '=':
+            putSymbolToken("=", 9);
+            break;
+        case '(':
+            putSymbolToken("(", 15);
+            break;
+        case ')':
+            putSymbolToken(")", 16);
+            break;
+        case ',':
+            putSymbolToken(",", 17);
+            break;
+        case '.':
+            putSymbolToken(".", 19);
+            break;
+        case '<':
+            putSymbolToken("<", 11);
+            break;
+        case '>':
+            putSymbolToken(">", 13);
+            break;
+        case ';':
+            putSymbolToken(";", 18);
+            break;
+        case ':':
+            putSymbolToken(":=", 9);
+            break;
+    }
+}
+
+
+void putIdentifierToken(char *string)
+{
+    tokenArray[structIndex].word = string;
+    tokenArray[structIndex].sym = 2;
+}
+
+void putNumToken(char *string)
+{
+    tokenArray[structIndex].word = string;
+    tokenArray[structIndex].sym = 3;
+}
+
+void putReserveToken(char *string, int symbol)
+{
+    tokenArray[structIndex].word = string;
+    tokenArray[structIndex].sym = symbol;
+}
+
+void putSymbolToken(char *string, int symbol)
+{
+    tokenArray[structIndex].word = string;
+    tokenArray[structIndex].sym = symbol;
 }
